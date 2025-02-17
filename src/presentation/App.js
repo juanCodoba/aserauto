724f724ddf77d1f75174069db37b337acbe7fe42
JulyanaLozano <josephtemi@hotmail.com>
2025-02-17 14:48:26 -0500

    configuracion pruebas

--- src/presentation/App.js
+++ src/presentation/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginView from '../presentation/views/LoginView';
 import ChatView from '../presentation/views/ChatView';
 import { AuthProvider } from '../core/auth/AuthContext'; // Importa el AuthProvider
 
 function App() {
   return (
-    <AuthProvider> {/* Envuelve las rutas con AuthProvider */}
-      <Routes>
-        <Route path="/" element={<LoginView />} />
-        <Route path="/chat" element={<ChatView />} />
-      </Routes>
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<LoginView />} />
+          <Route path="/chat" element={<ChatView />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
