44f9364e0206d770ee9fe44c050fc12cfc9c969c
JulyanaLozano <josephtemi@hotmail.com>
2025-02-17 17:28:19 -0500

    configuracion pruebas

--- src/presentation/App.js
+++ src/presentation/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'; // Añade Navigate
 import LoginView from '../presentation/views/LoginView';
 import ChatView from '../presentation/views/ChatView';
-import { AuthProvider } from '../core/auth/AuthContext'; // Importa el AuthProvider
+import { AuthProvider } from '../core/auth/AuthContext';
 
 function App() {
   return (
     <BrowserRouter basename="/aserauto">
       <AuthProvider>
         <Routes>
+          <Route path="/" element={<Navigate to="/login" />} /> {/* Redirige a /login */}
           <Route path="/login" element={<LoginView />} />
           <Route path="/chat" element={<ChatView />} />
         </Routes>
@@ -16,4 +17,5 @@ function App() {
     </BrowserRouter>
   );
 }
+
 export default App;
