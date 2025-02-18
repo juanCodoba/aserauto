c124e6f9d559156e93ed6f88f4a66e3e559d0b50
JulyanaLozano <josephtemi@hotmail.com>
2025-02-17 20:33:44 -0500

    Cambio de enrutador

--- src/presentation/App.js
+++ src/presentation/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'; // Añade Navigate
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'; // Cambia BrowserRouter a HashRouter
 import LoginView from '../presentation/views/LoginView';
 import ChatView from '../presentation/views/ChatView';
 import { AuthProvider } from '../core/auth/AuthContext';
 
 function App() {
   return (
-    <BrowserRouter basename="/aserauto">
+    <HashRouter> {/* Usa HashRouter en lugar de BrowserRouter */}
       <AuthProvider>
         <Routes>
-          <Route path="/" element={<Navigate to="/login" />} /> {/* Redirige a /login */}
+          <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<LoginView />} />
           <Route path="/chat" element={<ChatView />} />
         </Routes>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
