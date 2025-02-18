91b66744cfb86dfb3b313635ced5b3f203174ab7
JulyanaLozano <josephtemi@hotmail.com>
2025-02-17 21:23:13 -0500

    actualización 2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './presentation/App';
-import { HashRouter } from 'react-router-dom'; // Cambia BrowserRouter a HashRouter
+import { HashRouter } from 'react-router-dom'; // Solo aquí
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter> {/* Usa HashRouter en lugar de BrowserRouter */}
+    <HashRouter> {/* Solo aquí */}
       <App />
     </HashRouter>
   </React.StrictMode>
--- src/presentation/App.js
+++ src/presentation/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'; // Cambia BrowserRouter a HashRouter
+import { Routes, Route, Navigate } from 'react-router-dom'; // Quita HashRouter
 import LoginView from '../presentation/views/LoginView';
 import ChatView from '../presentation/views/ChatView';
 import { AuthProvider } from '../core/auth/AuthContext';
 
 function App() {
   return (
-    <HashRouter> {/* Usa HashRouter en lugar de BrowserRouter */}
-      <AuthProvider>
-        <Routes>
-          <Route path="/" element={<Navigate to="/login" />} />
-          <Route path="/login" element={<LoginView />} />
-          <Route path="/chat" element={<ChatView />} />
-        </Routes>
-      </AuthProvider>
-    </HashRouter>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LoginView />} />
+        <Route path="/chat" element={<ChatView />} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
