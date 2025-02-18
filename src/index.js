78e77af7aac86d3961ed1d8bc2c9aa6465d28b9c
JulyanaLozano <josephtemi@hotmail.com>
2025-02-17 21:35:04 -0500

    actualización 2

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './presentation/App';
-import { HashRouter } from 'react-router-dom'; // Solo aquí
+import { BrowserRouter } from 'react-router-dom'; // Cambiar a BrowserRouter
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter> {/* Solo aquí */}
+    <BrowserRouter> {/* Cambiar a BrowserRouter */}
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
